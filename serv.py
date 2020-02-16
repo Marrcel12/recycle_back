dd3e21b4263dd6907adeb8ccd2347c03855d6e08
mnn <nn>
2020-02-16 09:33:18 +0100

    dodanie obsługi i czytania linków

--- serv.py
+++ serv.py
@@ -1,5 +1,28 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+# obrabianie url
+def url_to_list(url):
+    url_to_process=url[int(str(url)[1:].find("/"))+2:]
+    flag=0
+    list={}
+    key=""
+    value=""
+    for i in url_to_process:
+            if i =="=" or i == "%":
+                    if i == "=":
+                        list[key]=""
+                        flag=1
+                    if i== "%":
+                        list[key]=value
+                        key=""
+                        value=""
+                        flag=0
+            else:
+                    if flag ==0:
+                        key+=i
+                    if flag ==1:
+                        value+=i
+    return(list)
 class Serv(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
@@ -10,10 +33,9 @@ class Serv(BaseHTTPRequestHandler):
         url=self.path
         # scan
         if  'scan' in url:
-            # search 
-            code= url.split("/")[2]
-           
-            # request do bazy
+            requests=url_to_list(url) 
+            print(requests)         
+            #  database
 
             materials=['dummy','dumm2']
             bins=['dummy','dumm2']
@@ -21,24 +43,49 @@ class Serv(BaseHTTPRequestHandler):
 
             self._set_headers()
             # send
-            self.wfile.write(json.dumps({'hello': code , 'materials': materials}).encode("utf-8"))
+            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
 
 
         #upcyling
         if  'upcyling' in url:
-            print('upcyling')
+            
+            requests=url_to_list(url) 
+            print(requests)
+            # database
+            tags=""
+            number_of_requests=0
+            photo_url=""
+            name=""
+            #json
+            self._set_headers()
+            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
 
         # facts
         if  'facts' in url:
+            requests=url_to_list(url) 
+            print(requests)
+            # database
             print('facts')
+            #json_send
+            self._set_headers()
+            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
+
 
         # goals
         if  'goals' in url:
+            requests=url_to_list(url) 
             print('goals')
+             #json_send
+            self._set_headers()
+            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
 
         # user
         if  'user' in url:
+            requests=url_to_list(url) 
             print('user')
+             #json_send
+            self._set_headers()
+            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
 
 
 httpd = HTTPServer(('localhost', 8080), Serv)
