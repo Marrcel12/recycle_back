fdf4a2a898da2ff4f1c5a2dd281c5b1278e4ca43
Unknown <marrcel12.marga@gmail.com>
2020-03-06 22:20:12 +0100

    dodanie obsługi serwera

--- serv.py
+++ serv.py
@@ -1,92 +1,110 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import json
-# obrabianie url
-def url_to_list(url):
-    url_to_process=url[int(str(url)[1:].find("/"))+2:]
-    flag=0
-    list={}
-    key=""
-    value=""
-    for i in url_to_process:
-            if i =="=" or i == "%":
-                    if i == "=":
-                        list[key]=""
-                        flag=1
-                    if i== "%":
-                        list[key]=value
-                        key=""
-                        value=""
-                        flag=0
-            else:
-                    if flag ==0:
-                        key+=i
-                    if flag ==1:
-                        value+=i
-    return(list)
-class Serv(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
+# from http.server import HTTPServer, BaseHTTPRequestHandler
+# import json
+# connect to the db
+import psycopg2
+con = psycopg2.connect(
+    host="localhost",
+    database="recycle",
+    user ='postgres',
+    password="qaz123",
+)
+# cursor
+cur = con.cursor()
+cur.execute("select name, code from scans")
+rows= cur.fetchall()
+for r in rows:
+    print(f" code: {r[0]}  code: {r[1]}" )
+cur.close()
+# con close
+con.close()
 
-    def do_GET(self):
-        url=self.path
-        # scan
-        if  'scan' in url:
-            requests=url_to_list(url) 
-            print(requests)         
-            #  database
+# # obrabianie url
+# def url_to_list(url):
+#     url_to_process=url[int(str(url)[1:].find("/"))+2:]
+#     flag=0
+#     list={}
+#     key=""
+#     value=""
+#     for i in url_to_process:
+#             if i =="=" or i == "%":
+#                     if i == "=":
+#                         list[key]=""
+#                         flag=1
+#                     if i== "%":
+#                         list[key]=value
+#                         key=""
+#                         value=""
+#                         flag=0
+#             else:
+#                     if flag ==0:
+#                         key+=i
+#                     if flag ==1:
+#                         value+=i
+#     return(list)
+# class Serv(BaseHTTPRequestHandler):
+#     def _set_headers(self):
+#         self.send_response(200)
+#         self.send_header('Content-type', 'application/json')
+#         self.end_headers()
 
-            materials=['dummy','dumm2']
-            bins=['dummy','dumm2']
-            name="lama"
+#     def do_GET(self):
+#         url=self.path
+#         # scan
+#         if  'scan' in url:
+#             requests=url_to_list(url) 
+#             print(requests)         
+#             #  database
 
-            self._set_headers()
-            # send
-            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
+#             materials=['dummy','dumm2']
+#             bins=['dummy','dumm2']
+#             name="lama"
+
+#             self._set_headers()
+#             # send
+#             self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
 
 
-        #upcyling
-        if  'upcyling' in url:
+#         #upcyling
+#         if  'upcyling' in url:
             
-            requests=url_to_list(url) 
-            print(requests)
-            # database
-            tags=""
-            number_of_requests=0
-            photo_url=""
-            name=""
-            #json
-            self._set_headers()
-            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
+#             requests=url_to_list(url) 
+#             print(requests)
+#             # database
+#             tags=""
+#             number_of_requests=0
+#             photo_url=""
+#             name=""
+#             #json
+#             self._set_headers()
+#             self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
 
-        # facts
-        if  'facts' in url:
-            requests=url_to_list(url) 
-            print(requests)
-            # database
-            print('facts')
-            #json_send
-            self._set_headers()
-            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
-
-
-        # goals
-        if  'goals' in url:
-            requests=url_to_list(url) 
-            print('goals')
-             #json_send
-            self._set_headers()
-            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
-
-        # user
-        if  'user' in url:
-            requests=url_to_list(url) 
-            print('user')
-             #json_send
-            self._set_headers()
-            self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
+#         # facts
+#         if  'facts' in url:
+#             requests=url_to_list(url) 
+#             print(requests)
+#             # database
+#             print('facts')
+#             #json_send
+#             self._set_headers()
+#             self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
 
 
-httpd = HTTPServer(('localhost', 8080), Serv)
-httpd.serve_forever()
+#         # goals
+#         if  'goals' in url:
+#             requests=url_to_list(url) 
+#             print('goals')
+#              #json_send
+#             self._set_headers()
+#             self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
+
+#         # user
+#         if  'user' in url:
+#             requests=url_to_list(url) 
+#             print('user')
+#              #json_send
+#             self._set_headers()
+#             self.wfile.write(json.dumps({'hello': 'Test1' }).encode("utf-8"))
+
+
+# httpd = HTTPServer(('localhost', 8080), Serv)
+# httpd.serve_forever()
